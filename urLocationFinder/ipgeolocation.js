e20784d9135eb4679de558af3b0df50e7b6aa65c
Marvellous Amasi <102649743+Marveeamasi@users.noreply.github.com>
2025-01-06 11:37:32 +0100

    Update ipgeolocation.js

--- urLocationFinder/ipgeolocation.js
+++ urLocationFinder/ipgeolocation.js
@@ -1,6 +1,6 @@
 const findMeButton = document.getElementById("findMe");
 const output = document.getElementById("output");
-
+const reStart = () => output.innerHTML = "Again, your location details will appear here...";
 const API_KEY = "477b04ba40ce487b99984aca5c47b2a0";
 
 const fetchLocation = async () => {
@@ -18,8 +18,12 @@ const fetchLocation = async () => {
         <p><strong>Latitude:</strong> ${latitude}</p>
         <p><strong>Longitude:</strong> ${longitude}</p>
         <p><strong>ISP:</strong> ${isp}</p>
+        <img src="https://www.iconpacks.net/icons/2/free-refresh-icon-3104-thumb.png" style="width: 30px; height: auto"
+        onclick="reStart()"
+        >
       </div>
     `;
+
   } catch (error) {
     output.innerHTML = `<div class="error">Error: ${error.message}</div>`;
   }
